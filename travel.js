faf3b6a6abe642d41c8a00e1b9bc7a9410abd7fe
Laura <laura.bolea.mail@gmail.com>
2019-03-25 17:10:24 +0100

    I did itgit add .

---
+++ travel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Travel extends Component {
+  render(){
+    return (
+      <div>
+        <h1>My destination is: {this.props.destination} </h1>
+        <h2>my country is: {this.props.country} </h2>
+        <img src= {this.props.image}></img>
+        <h3>distance: {this.props.distance}  </h3>
+      </div>
+    )
+  }
+}
+
+
+export default Travel;
